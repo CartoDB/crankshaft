0742e3f72a7002652163880ad2d80e9d34a6c5d8
Javier Goizueta <jgoizueta@gmail.com>
2016-02-18 16:10:34 +0100

    Rename cdb_random_seeds as 'internal'

--- pg/sql/0.0.1/01_random_seeds.sql
+++ pg/sql/0.0.1/01_random_seeds.sql
@@ -1,7 +1,8 @@
+-- Internal function.
 -- Set the seeds of the RNGs (Random Number Generators)
 -- used internally.
 CREATE OR REPLACE FUNCTION
-cdb_random_seeds (seed_value INTEGER) RETURNS VOID
+_cdb_random_seeds (seed_value INTEGER) RETURNS VOID
 AS $$
   from crankshaft import random_seeds
   random_seeds.set_random_seeds(seed_value)
--- pg/test/0.0.1/sql/02_moran_test.sql
+++ pg/test/0.0.1/sql/02_moran_test.sql
@@ -4,7 +4,7 @@
 -- Moral functions perform some nondeterministic computations
 -- (to estimate the significance); we will set the seeds for the RNGs
 -- that affect those results to have repeateble results
-SELECT cdb_crankshaft.cdb_random_seeds(1234);
+SELECT cdb_crankshaft._cdb_random_seeds(1234);
 
 SELECT ppoints.code, m.quads
   FROM ppoints
@@ -12,7 +12,7 @@ SELECT ppoints.code, m.quads
     ON ppoints.cartodb_id = m.ids
   ORDER BY ppoints.code;
 
-SELECT cdb_crankshaft.cdb_random_seeds(1234);
+SELECT cdb_crankshaft._cdb_random_seeds(1234);
 
 SELECT ppoints2.code, m.quads
   FROM ppoints2
