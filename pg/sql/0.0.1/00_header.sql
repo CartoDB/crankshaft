ef17e2fe4ceb4d18f985f47893e254bbf5b9be95
Javier Goizueta <jgoizueta@gmail.com>
2016-02-22 16:14:52 +0100

    Add header

---
+++ pg/sql/0.0.1/00_header.sql
@@ -0,0 +1,3 @@
+--DO NOT MODIFY THIS FILE, IT IS GENERATED AUTOMATICALLY FROM SOURCES
+-- Complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION crankshaft" to load this file. \quit
