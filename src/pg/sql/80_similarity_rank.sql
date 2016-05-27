45705f3a16ed34d14ca49c09898c5f801d5c985e
Stuart Lynn <stuart.lynn@gmail.com>
2016-05-27 10:29:47 -0400

    adding function preflight

--- src/pg/sql/80_similarity_rank.sql
+++ src/pg/sql/80_similarity_rank.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE FUNCTION cdb_SimilarityRank(cartodb_id numeric, query text)
 returns TABLE (cartodb_id NUMERIC, similarity NUMERIC)
 as $$
+  plpy.execute('SELECT cdb_crankshaft._cdb_crankshaft_activate_py()')
   from crankshaft.similarity import similarity_rank
   return similarity_rank(cartodb_id, query)
 $$ LANGUAGE plpythonu
@@ -8,6 +9,7 @@ $$ LANGUAGE plpythonu
 CREATE OR REPLACE FUNCTION cdb_MostSimilar(cartodb_id numeric, query text ,matches numeric)
 returns TABLE (cartodb_id NUMERIC, similarity NUMERIC)
 as $$
+  plpy.execute('SELECT cdb_crankshaft._cdb_crankshaft_activate_py()')
   from crankshaft.similarity import most_similar
   return most_similar(matches, query)
 $$ LANGUAGE plpythonu
