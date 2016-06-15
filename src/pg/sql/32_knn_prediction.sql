99efa5dc3ff1626d1f4e32ac21a855f55c1f94c9
Andy Eschbacher <andy.eschbacher@gmail.com>
2016-06-15 14:57:59 -0400

    adding first iteration of script

---
+++ src/pg/sql/32_knn_prediction.sql
@@ -0,0 +1,46 @@
+
+CREATE OR REPLACE FUNCTION CDB_knnWeightedAverage(source_geom geometry(Point, 4326), target_geoms geometry(Point, 4326)[], target_vals numeric[], num_neighbors INT)
+RETURNS numeric
+AS $$
+DECLARE
+  weighted_avg numeric;
+  vals numeric[];
+  distances numeric[];
+  idx INT;
+BEGIN
+
+  EXECUTE
+  'SELECT array_agg(vals), array_agg(ST_Distance($1::geography, geoms::geography)) As dist
+  FROM (
+    SELECT geoms, vals FROM (
+      SELECT unnest($2) As geoms, unnest($3) As vals
+    ) As i
+    ORDER BY $1 <-> geoms
+    LIMIT 5
+  ) As j'
+  USING source_geom, target_geoms, target_vals
+  INTO vals, distances;
+
+  IF 0 = Any(distances)
+  THEN
+    FOR idx IN 1..array_length(distances, 1)
+    LOOP
+      IF distances[idx] = 0
+      THEN
+        weighted_avg := vals[idx];
+        RAISE DEBUG 'zero distance at %, returning val %', idx, weighted_avg;
+        RETURN weighted_avg;
+      END IF;
+    END LOOP;
+  ELSE
+    weighted_avg := (SELECT sum(v / d) / sum(1.0 / d) FROM (SELECT unnest(vals) As v, unnest(distances) As d) As x);
+    RETURN weighted_avg;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+-- SELECT
+--   sum(val / ST_Distance(source_geom::geography, t.g::geography)) / sum(1.0 / ST_Distance(source_geom::geography, t.g::geography))
+-- FROM target_geoms As t(g)
+-- ORDER BY j.{geom_col} <-> i.{geom_col} ASC
+-- LIMIT num_neighbors
