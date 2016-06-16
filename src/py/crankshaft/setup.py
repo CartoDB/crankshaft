237aa1c5818f003ffb459817ea5e72392c765c5c
Raul Ochoa <rochoaf@gmail.com>
2016-06-16 16:34:45 +0200

    Declare scipy as dep

--- src/py/crankshaft/setup.py
+++ src/py/crankshaft/setup.py
@@ -40,7 +40,7 @@ setup(
 
     # The choice of component versions is dictated by what's
     # provisioned in the production servers.
-    install_requires=['numpy==1.11.0', 'pysal==1.9.1', 'scikit-learn==0.17.1'],
+    install_requires=['numpy==1.11.0', 'scipy==0.17.1', 'pysal==1.9.1', 'scikit-learn==0.17.1'],
 
     requires=['pysal', 'numpy', 'sklearn'],
 
