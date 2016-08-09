358202d324e1deef8f0714f896ff7226257b4b73
abelvm <abel@cartodb.com>
2016-08-09 13:03:24 -0400

    check tests

--- src/pg/sql/15_tinmap.sql
+++ src/pg/sql/15_tinmap.sql
@@ -16,7 +16,7 @@ DECLARE
     vc numeric;
     coltemp numeric[];
 BEGIN
-    SELECT array_agg(dens.geomout), array_agg(dens.colout) INTO p, vals FROM CDB_Densify(geomin, colin, iterations) dens;
+    SELECT array_agg(dens.geomout), array_agg(dens.colout) INTO p, vals FROM cdb_crankshaft.CDB_Densify(geomin, colin, iterations) dens;
     WITH    a as (SELECT unnest(p) AS e),
             b as (SELECT ST_DelaunayTriangles(ST_Collect(a.e),0.001, 0) AS t FROM a),
             c as (SELECT (ST_Dump(t)).geom AS v FROM b)
