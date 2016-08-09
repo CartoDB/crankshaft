8c273a53bcf8ff0b90b360d9e3cfe84d45785afa
abelvm <abel@cartodb.com>
2016-08-09 13:01:05 -0400

    check tests

--- src/pg/test/sql/09_voronoi_test.sql
+++ src/pg/test/sql/09_voronoi_test.sql
@@ -3,5 +3,5 @@ WITH a AS (
     ARRAY[ST_GeomFromText('POINT(2.1744 41.403)'),ST_GeomFromText('POINT(2.1228 41.380)'),ST_GeomFromText('POINT(2.1511 41.374)'),ST_GeomFromText('POINT(2.1528 41.413)'),ST_GeomFromText('POINT(2.165 41.391)'),ST_GeomFromText('POINT(2.1498 41.371)'),ST_GeomFromText('POINT(2.1533 41.368)'),ST_GeomFromText('POINT(2.131386 41.41399)')] AS geomin
 )
 SELECT
-    CDB_voronoi(geomin, 0.2, 1e-9) as result
+    cdb_crankshaft.CDB_voronoi(geomin, 0.2, 1e-9) as result
 FROM a;
