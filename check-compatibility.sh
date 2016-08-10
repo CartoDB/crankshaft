9329f130b4a3caa5dde4c572263e96b6985f1d2d
Rafa de la Torre <rtorre@cartodb.com>
2016-08-10 17:14:27 +0200

    Move psql input from stdin to a file
    This seems to be affecting builds causing a freeze.

--- check-compatibility.sh
+++ check-compatibility.sh
@@ -24,29 +24,7 @@ trap cleanup EXIT
 psql -c "SELECT * FROM pg_available_extension_versions WHERE name LIKE 'crankshaft';"
 
 # Install in the fresh DB
-psql $DBNAME <<'EOF'
--- Install dependencies
-CREATE EXTENSION plpythonu;
-CREATE EXTENSION postgis VERSION '2.2.2';
-
--- Create role publicuser if it does not exist
-DO
-$$
-BEGIN
-   IF NOT EXISTS (
-      SELECT *
-      FROM   pg_catalog.pg_user
-      WHERE  usename = 'publicuser') THEN
-
-      CREATE ROLE publicuser LOGIN;
-   END IF;
-END
-$$ LANGUAGE plpgsql;
-
--- Install the default version
-CREATE EXTENSION crankshaft;
-\dx
-EOF
+psql $DBNAME -f ci/create-default-extension.sql
 
 # TODO save public functions and signatures
 
