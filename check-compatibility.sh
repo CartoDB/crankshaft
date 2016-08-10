da92552bf54587445f2e83ac55da610e23667c7f
Rafa de la Torre <rtorre@cartodb.com>
2016-08-10 17:48:55 +0200

    Comment out "offending" line

--- check-compatibility.sh
+++ check-compatibility.sh
@@ -22,7 +22,7 @@ trap cleanup EXIT
 (cd src/py && sudo make deploy RUN_OPTIONS="--no-deps") || die "Could not deploy python extension"
 (cd src/pg && sudo make deploy) || die " Could not deploy last release"
 echo "[rtorre] is it even getting here???"
-psql -c "SELECT * FROM pg_available_extension_versions WHERE name LIKE 'crankshaft';"
+#psql -c "SELECT * FROM pg_available_extension_versions WHERE name LIKE 'crankshaft';"
 
 echo "[rtorre] so far so good"
 
