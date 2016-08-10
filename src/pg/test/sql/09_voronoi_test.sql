613cdb4c6fdd1dd0bad6d86fe4a80722075a7c12
abelvm <abel@cartodb.com>
2016-08-10 12:15:50 -0400

    fixed tests

--- src/pg/test/sql/09_voronoi_test.sql
+++ src/pg/test/sql/09_voronoi_test.sql
@@ -1,3 +1,6 @@
+SET client_min_messages TO WARNING;
+\set ECHO none
+
 WITH a AS (
     SELECT
     ARRAY[ST_GeomFromText('POINT(2.1744 41.403)'),ST_GeomFromText('POINT(2.1228 41.380)'),ST_GeomFromText('POINT(2.1511 41.374)'),ST_GeomFromText('POINT(2.1528 41.413)'),ST_GeomFromText('POINT(2.165 41.391)'),ST_GeomFromText('POINT(2.1498 41.371)'),ST_GeomFromText('POINT(2.1533 41.368)'),ST_GeomFromText('POINT(2.131386 41.41399)')] AS geomin
