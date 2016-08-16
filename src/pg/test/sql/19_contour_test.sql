0ea060a69816a2318a77d8c7e23579ed90601a92
abelvm <abel@cartodb.com>
2016-08-16 13:56:01 -0400

    added srid to tests

--- src/pg/test/sql/19_contour_test.sql
+++ src/pg/test/sql/19_contour_test.sql
@@ -5,7 +5,7 @@ SET client_min_messages TO WARNING;
 WITH a AS (
     SELECT
     ARRAY[800, 700, 600, 500, 400, 300, 200, 100]::numeric[] AS vals,
-    ARRAY[ST_GeomFromText('POINT(2.1744 41.403)'),ST_GeomFromText('POINT(2.1228 41.380)'),ST_GeomFromText('POINT(2.1511 41.374)'),ST_GeomFromText('POINT(2.1528 41.413)'),ST_GeomFromText('POINT(2.165 41.391)'),ST_GeomFromText('POINT(2.1498 41.371)'),ST_GeomFromText('POINT(2.1533 41.368)'),ST_GeomFromText('POINT(2.131386 41.41399)')] AS g
+    ARRAY[ST_GeomFromText('POINT(2.1744 41.403)',4326),ST_GeomFromText('POINT(2.1228 41.380)',4326),ST_GeomFromText('POINT(2.1511 41.374)',4326),ST_GeomFromText('POINT(2.1528 41.413)',4326),ST_GeomFromText('POINT(2.165 41.391)',4326),ST_GeomFromText('POINT(2.1498 41.371)',4326),ST_GeomFromText('POINT(2.1533 41.368)',4326),ST_GeomFromText('POINT(2.131386 41.41399)',4326)] AS g
 ),
 b as(
 SELECT
