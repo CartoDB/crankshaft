2ede55d1658e3a499990eb7a5c7b88a69b480c97
Andy Eschbacher <andy.eschbacher@gmail.com>
2016-09-19 12:17:01 -0400

    pep8 updates

--- src/py/crankshaft/crankshaft/clustering/getis.py
+++ src/py/crankshaft/crankshaft/clustering/getis.py
@@ -11,12 +11,13 @@ import crankshaft.pysal_utils as pu
 
 # High level interface ---------------------------------------
 
+
 def getis_ord(subquery, attr,
               w_type, num_ngbrs, permutations, geom_col, id_col):
     """
     Getis-Ord's G*
-    Implementation building neighbors with a PostGIS database and PySAL's Getis-Ord's G*
-     hotspot/coldspot module.
+    Implementation building neighbors with a PostGIS database and PySAL's
+      Getis-Ord's G* hotspot/coldspot module.
     Andy Eschbacher
     """
 
@@ -41,11 +42,11 @@ def getis_ord(subquery, attr,
 
     attr_vals = pu.get_attributes(result)
 
-    ## build PySAL weight object
+    # build PySAL weight object
     weight = pu.get_weight(result, w_type, num_ngbrs)
 
     # calculate Getis-Ord's G* z- and p-values
     getis = ps.esda.getisord.G_Local(attr_vals, weight,
-      star=True, permutations=permutations)
+                                     star=True, permutations=permutations)
 
     return zip(getis.z_sim, getis.p_sim, getis.p_z_sim, weight.id_order)
