3e1cef9958984e85ed5a6f859f7cb9374c50d9cb
Andy Eschbacher <andy.eschbacher@gmail.com>
2016-10-11 16:48:22 -0400

    fix output signature

--- src/pg/sql/11_kmeans.sql
+++ src/pg/sql/11_kmeans.sql
@@ -12,7 +12,7 @@ $$ LANGUAGE plpythonu;
 -- query: sql query to retrieve all the needed data
 
 CREATE OR REPLACE FUNCTION CDB_KMeansNonspatial(query TEXT, col_names TEXT[], no_clusters INTEGER, id_col TEXT DEFAULT 'cartodb_id')
-RETURNS TABLE(rowid BIGINT, cluster_no INTEGER, )
+RETURNS TABLE(cluster_label text, cluster_center text, rowid bigint)
 
 from crankshaft.clustering import kmeans_nonspatial
 return kmeans_nonspatial(query, colnames, num_clusters, id_col)
