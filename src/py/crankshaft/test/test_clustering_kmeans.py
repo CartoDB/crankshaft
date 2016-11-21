2f27622a6dbbe44d068bdbf7c1672d74112399b7
Andy Eschbacher <andy.eschbacher@gmail.com>
2016-11-21 16:19:54 +0000

    strips out kmeans non spatial

--- src/py/crankshaft/test/test_clustering_kmeans.py
+++ src/py/crankshaft/test/test_clustering_kmeans.py
@@ -54,36 +54,3 @@ class KMeansTest(unittest.TestCase):
         self.assertEqual(len(np.unique(labels)), 2)
         self.assertEqual(len(c1), 20)
         self.assertEqual(len(c2), 20)
-
-
-class KMeansNonspatialTest(unittest.TestCase):
-    """Testing class for k-means non-spatial"""
-
-    def setUp(self):
-        self.params = {"subquery": "SELECT * FROM TABLE",
-                       "n_clusters": 5}
-
-    def test_kmeans_nonspatial(self):
-        """
-            test for k-means non-spatial
-        """
-        # data from:
-        # http://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html#sklearn-cluster-kmeans
-        data_raw = [OrderedDict([("col1", [1, 1, 1, 4, 4, 4]),
-                                 ("col2", [2, 4, 0, 2, 4, 0]),
-                                 ("rowids", [1, 2, 3, 4, 5, 6])])]
-
-        random_seeds.set_random_seeds(1234)
-        kmeans = Kmeans(FakeQueryRunner(data_raw))
-        print 'asfasdfasd'
-        clusters = kmeans.nonspatial('subquery', ['col1', 'col2'], 2)
-        print str([c[0] for c in clusters])
-
-        cl1 = clusters[0][0]
-        cl2 = clusters[3][0]
-
-        for idx, val in enumerate(clusters):
-            if idx < 3:
-                self.assertEqual(val[0], cl1)
-            else:
-                self.assertEqual(val[0], cl2)
