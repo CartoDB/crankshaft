666075b0055d0f7741ea733d371de8f0cb320abf
Taylor Oshan <tayoshan@gmail.com>
2016-12-19 22:14:39 -0700

    forgot to change import after copying over new file

--- src/py/crankshaft/crankshaft/regression/spint/base/gravity.py
+++ src/py/crankshaft/crankshaft/regression/spint/base/gravity.py
@@ -20,7 +20,7 @@ import numpy as np
 from scipy import sparse as sp
 from pysal.spreg import user_output as User
 from pysal.spreg.utils import sphstack
-from pysal.contrib.glm.utils import cache_readonly
+from crankshaft.regression.glm.utils import cache_readonly
 from count_model import CountModel
 from utils import sorensen, srmse, spcategorical
 
