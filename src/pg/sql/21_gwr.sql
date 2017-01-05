ba54e9b42d257b7f10b669b7c04904d8710de5a0
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-01-05 14:24:10 -0500

    consolidate gwr sql functions

--- src/pg/sql/21_gwr.sql
+++ src/pg/sql/21_gwr.sql
@@ -11,3 +11,17 @@ gwr = GWR()
 return gwr.gwr(subquery, dep_var, ind_vars, bw, fixed, kernel)
 
 $$ LANGUAGE plpythonu;
+
+
+CREATE OR REPLACE FUNCTION
+CDB_GWR_Predict(subquery text, dep_var text, ind_vars text[],
+                bw numeric default null, fixed boolean default False,
+                kernel text default 'bisquare')
+RETURNS table(coeffs JSON, stand_errs JSON, t_vals JSON, r_squared numeric, predicted numeric, rowid bigint)
+AS $$
+
+from crankshaft.regression import gwr_cs
+
+return gwr_cs.gwr_predict(subquery, dep_var, ind_vars, bw, fixed, kernel)
+
+$$ LANGUAGE plpythonu;
