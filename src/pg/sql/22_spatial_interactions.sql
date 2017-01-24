6c081139ba96e74876b833a21f1325b53a334fa5
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-01-24 11:07:55 -0500

    change to lowercase args

--- src/pg/sql/22_spatial_interactions.sql
+++ src/pg/sql/22_spatial_interactions.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION
 CDB_Gravity(subquery text, flows text, o_vars text[], d_vars text[],
-       cost text, cost_func text default 'pow', Quasi boolean default False)
+       cost text, cost_func text default 'pow', quasi boolean default False)
 RETURNS table(coeffs JSON, stand_errs JSON, t_vals JSON, predicted numeric, r_squared numeric, aic numeric, rowid varchar)
 AS $$
 
