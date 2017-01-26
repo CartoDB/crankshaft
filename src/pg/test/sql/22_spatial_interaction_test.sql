aea68c8b078c730675676e588b1ee21c14142c22
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-01-25 21:26:49 -0500

    adds tests and expectations for all functions

--- src/pg/test/sql/22_spatial_interaction_test.sql
+++ src/pg/test/sql/22_spatial_interaction_test.sql
@@ -97,15 +97,33 @@ LIMIT 2;
 
 
 -- Local Production-constrained
+\echo '--> local gravity'
+SELECT
+  a.cartodb_id,
+  a.flow_data,
+  round((spint.coeffs->>'origin_i')::numeric, 4) As coeff_origin_i,
+  round((spint.coeffs->>'destination_j')::numeric, 4) As coeff_destination_j,
+  round((spint.coeffs->>'dij')::numeric, 4) As coeff_cost,
+  round(spint.r_squared, 4) As r_squared,
+  round(spint.aic, 4) as aic
+FROM cdb_crankshaft.CDB_SpIntLocalGravity('select * from austria_test',
+  'flow_data',
+  Array['origin_i'],
+  Array['destination_j'],
+  'origin',
+  'dij') As spint
+JOIN austria_test As a
+on a.cartodb_id = spint.rowid
+ORDER BY a.cartodb_id ASC
+LIMIT 10;
+
+
+-- Local Production-constrained
 \echo '--> local production-constrained'
 SELECT
   a.cartodb_id,
-  -- spint.coeffs::text,
-  -- spint.predicted,
   a.flow_data,
-  round((spint.coeffs->>'dest_25741')::numeric, 4) As coeff_dest_25741,
-  round((spint.coeffs->>'origin_4897')::numeric, 4) As coeff_origin_4897,
-  round((spint.coeffs->>'intercept')::numeric, 4) As coeff_intercept,
+  round((spint.coeffs->>'destination_j')::numeric, 4) As coeff_destination_j,
   round((spint.coeffs->>'dij')::numeric, 4) As coeff_cost,
   round(spint.r_squared, 4) As r_squared,
   round(spint.aic, 4) as aic
@@ -117,4 +135,23 @@ FROM cdb_crankshaft.CDB_SpIntLocalProduction('select * from austria_test',
 JOIN austria_test As a
 on a.cartodb_id = spint.rowid
 ORDER BY a.cartodb_id ASC
-LIMIT 2;
+LIMIT 10;
+
+-- Local Production-constrained
+\echo '--> local attraction-constrained'
+SELECT
+  a.cartodb_id,
+  a.flow_data,
+  round((spint.coeffs->>'origin_i')::numeric, 4) As coeff_origin_i,
+  round((spint.coeffs->>'dij')::numeric, 4) As coeff_cost,
+  round(spint.r_squared, 4) As r_squared,
+  round(spint.aic, 4) as aic
+FROM cdb_crankshaft.CDB_SpIntLocalAttraction('select * from austria_test',
+  'flow_data',
+  'destination',
+  Array['origin_i'],
+  'dij') As spint
+JOIN austria_test As a
+on a.cartodb_id = spint.rowid
+ORDER BY a.cartodb_id ASC
+LIMIT 10;
