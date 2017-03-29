dc8cc0e1c7ce9e47d737793a53a34fa13b09b4b7
Andy Eschbacher <andy.eschbacher@gmail.com>
2017-03-29 15:20:46 -0400

    expose model parameters

--- src/pg/sql/25_optimization.sql
+++ src/pg/sql/25_optimization.sql
@@ -3,14 +3,21 @@ CDB_OptimAssignments(drain text,
                      source text,
                      drain_capacity text,
                      source_production text,
-                     marginal_cost text)
+                     marginal_cost text,
+                     waste_per_person numeric DEFAULT 0.01,
+                     recycle_rate numeric DEFAULT 0.0,
+                     dist_rate numeric DEFAULT 0.15)
 RETURNS table(drain_id bigint, source_id int, cost numeric) AS $$
 
 from crankshaft.optimization import Optim
 
-optim = Optim(drain, source, drain_capacity, source_production, marginal_cost)
+params = {'waste_per_person': float(waste_per_person),
+          'recycle_rate': float(recycle_rate),
+          'dist_rate': float(dist_rate)}
+
+optim = Optim(drain, source, drain_capacity, source_production, marginal_cost,
+              **params)
 x = optim.output()
-print(x)
 
 return x
 $$ LANGUAGE plpythonu;
