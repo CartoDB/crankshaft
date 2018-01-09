20104c2df953d9bb2cac1ee91e473aaa9156d22e
Andy Eschbacher <andy.eschbacher@gmail.com>
2018-01-09 13:35:00 -0500

    adds sql tests for nonspatial kmeans

--- src/pg/test/sql/11_kmeans_test.sql
+++ src/pg/test/sql/11_kmeans_test.sql
@@ -1,6 +1,31 @@
 \pset format unaligned
 \set ECHO all
 
-SELECT count(DISTINCT cluster_no) as clusters from cdb_crankshaft.cdb_kmeans('select * from ppoints', 2);
+-- spatial kmeans
+SELECT
+    count(DISTINCT cluster_no) as clusters
+FROM
+    cdb_crankshaft.cdb_kmeans('select * from ppoints', 2);
 
-SELECT count(*) clusters from (select  cdb_crankshaft.CDB_WeightedMean(the_geom, value::NUMERIC), code from ppoints group by code) p;
+-- weighted mean
+SELECT
+    count(*) clusters
+FROM (
+    SELECT
+        cdb_crankshaft.CDB_WeightedMean(the_geom, value::NUMERIC),
+        code
+    FROM ppoints
+    GROUP BY code
+) p;
+
+-- nonspatial kmeans
+SELECT
+    cluster_label,
+    cluster_center,
+    silhouettes,
+    inertia,
+    rowid
+FROM cdb_crankshaft.CDB_KMeansNonspatial(
+    'select unnest(Array[1, 1, 10, 10]) as col1, unnest(100, 100, 2, 2) as col2 from ppoints',
+    Array['col1', 'col2']::text[],
+    2);
